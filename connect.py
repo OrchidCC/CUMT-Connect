292b640973dad9b3b4a283d6ecc2fdcd61e9f706
OrchidCC <105629723+OrchidCC@users.noreply.github.com>
2022-05-16 15:24:17 +0800

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,13 @@
+import requests
+from time import sleep
+
+f = open('account.txt', 'r', encoding='utf-8')
+lines = f.readlines()
+usn = lines[0].strip()
+psw = lines[1].strip()
+f.close()
+url = "http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=" + usn + "%40cmcc&user_password=" + psw
+r = requests.get(url)
+print(eval(r.content))
+sleep(3)
+exit()
